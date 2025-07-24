93b807cfadb2a2723c33c733b1602ae634616873
jihyun2256 <jihyun1905@gmail.com>
2025-07-24 15:38:15 +0900

    2507241538

---
+++ function18.py
@@ -0,0 +1,14 @@
+##function18.py
+
+#함수 안에서 전역 변수 사용하기 
+
+def calculate_area(radius):
+    global area
+    area = 3.14 * radius ** 2
+    return
+
+r = float(input('원의 반지름 : '))
+
+calculate_area(r)
+
+print(area)
